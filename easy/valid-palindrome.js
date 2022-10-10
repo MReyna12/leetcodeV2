b9afbc13571c556433796b801e7fde587365b4e7
Michael <mreyna12@live.com>
2022-10-10 15:18:51 -0500

    grind75 practice

---
+++ easy/valid-palindrome.js
@@ -0,0 +1,6 @@
+// Pseudocode:
+//  1. Create an object that contains letters a-z and numbers 0-9.
+//  2. Loop through the string, comparing the lowercase version of the character to the object. If the value exists, then add the character and 1 (or add 1 of already existing) to a new object. If the character does not exist in the object, then go to the next character.
+//  3. Loop through the object and if any of the values is an odd number, then return false.
+
+function isPalindrome(s) {}
