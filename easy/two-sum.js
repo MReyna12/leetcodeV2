c9b585f355c01e0274cdaa258566cb4a2e1c6e01
Michael <mreyna12@live.com>
2022-10-10 11:37:54 -0500

    grind75 practice

--- easy/two-sum.js
+++ easy/two-sum.js
@@ -1,16 +1,21 @@
 // P: first param - array of integers. second param - an integer target.
 // R: return the indices of the two numbers such that they add up to the target
 // E: [2, 7, 11, 15], target = 9. Should return [0, 1] because 2 + 7 = 9.
-// P: Create empty object; run a for loop that checks to see if target minus num in array exists(so the value of target - num will be the key), if not, then add number from array as key and i as the value (which represents the index).
-//    if it does exist, then return the object[target-num], i. So we would be returning the index position of the key/value pair in the object, plus the current num in the array based on i.
+// P:
 
 function twoSum(nums, target) {
+  // Create an empty object that will hold keys (numbers in the array) and values (their respective index position in said array)
   const numsObject = {};
 
+  // Run a for loop with a conditional. The conditional checks to see if target minus each num in the array exists in the numsObject. Ex: numsObject[target - nums[i]] !== undefined.
+  // If the key/value does exist in the object, then return the value and the current i value, which will represent the two indices that equate to the target.
   for (let i = 0; i < nums.length; i++) {
-    if (numsObject[target - nums[i]] !== undefined) {
-      return [numsObject[target - nums[i]], i];
+    const number = nums[i];
+
+    if (numsObject[target - number] !== undefined) {
+      return [numsObject[target - number], i];
     }
-    numsObject[nums[i]] = i;
+
+    numsObject[number] = i;
   }
 }
