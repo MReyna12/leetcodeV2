3d54107ef493f66e5832d2d8b129c3e3aa9586cc
Michael <mreyna12@live.com>
2022-10-11 09:54:08 -0500

    stack practice

--- easy/valid-parentheses.js
+++ easy/valid-parentheses.js
@@ -1,37 +1,29 @@
-// P:
-// R:
-// E:
-// P:
-
 function isValid(s) {
-  // If the input string is valid there must be an even number of characters (need to pair brackets together)
-  // Conditional that checks if the input string length is even
-  if (s.length % 2 === 1) return false;
+  // If string length is less than two, return false
+  if (s.length < 2) return false;
 
-  // Stack Method
-  // Create a stack that will hold the corresponding closing bracket of each open bracket that appears in the loop
+  // Create a stack that will hold closed brackets
   let stack = [];
 
-  // Create a bracket object that will determine which closing bracket goes into the stack
+  // Create a brackets object that sets the keys as the open brackets and their values as the respective closing brackets
   const brackets = {
     "(": ")",
     "[": "]",
     "{": "}",
   };
 
-  // For loop that checks each character of the string. If the character is an open bracket, then add its closing bracket to the stack. If the character is a closing bracket, confirm that the last closing bracket in the stack is the same as the character closing bracket.
+  // Create a for loop that will check two things: 1. if character === open bracket, then add it's closing bracket to the stack; or 2. if character === closing bracket, then confirm that the last closing bracket added to the stack matches said charcter.
+  // If it does not match, return false because that means the closing bracket is not in order. If it does match, then the bracket is removed from the stack and the loop continues.
   for (let i = 0; i < s.length; i++) {
-    const character = s[i];
+    const bracket = s[i];
 
-    // If open bracket, then add it's corresponding closing bracket to the stack array
-    if (brackets[character]) {
-      stack.push(brackets[character]);
-    } else if (character !== stack.pop()) {
-      // If closing bracket, then confirm the last bracket added to the stack is the same as the character (because brackets must be closed in order)
+    if (brackets[bracket]) {
+      stack.push(brackets[bracket]);
+    } else if (bracket !== stack.pop()) {
       return false;
     }
   }
 
-  // If all brackets are closed in the correct order, then the stack should be empty due to stack.pop() removing them from the stack
+  // If the stack is empty at the end of the loop, then return value is true.
   return stack.length === 0;
 }
