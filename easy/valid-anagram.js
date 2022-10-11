8d0ed921807aea32230f20b40e42a8c43dba43c1
Michael <mreyna12@live.com>
2022-10-11 10:08:18 -0500

    string practice

--- easy/valid-anagram.js
+++ easy/valid-anagram.js
@@ -1,20 +1,21 @@
 function isAnagram(s, t) {
+  // If both strings do not have the same length, then they cannot be an anagram.
   if (s.length !== t.length) return false;
 
-  const anagramObject = {};
+  // Create an object that will hold each character and the number of times they appear in the s string.
+  let characterCount = {};
 
+  // For loop that adds the characters from the s string to the characterCount object
   for (let i = 0; i < s.length; i++) {
-    if (anagramObject[s[i]]) {
-      anagramObject[s[i]] += 1;
-    } else {
-      anagramObject[s[i]] = 1;
-    }
+    characterCount[s[i]]
+      ? (characterCount[s[i]] += 1)
+      : (characterCount[s[i]] = 1);
   }
 
   for (const character of t) {
-    if (anagramObject[character]) {
-      anagramObject[character] -= 1;
-    } else if (!anagramObject[character]) {
+    if (characterCount[character]) {
+      characterCount[character] -= 1;
+    } else if (!characterCount[character]) {
       return false;
     }
   }
