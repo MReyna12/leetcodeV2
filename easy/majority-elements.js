51ad6b1a3c8c61cf7a63533474c3a8b40306d9a9
Michael <mreyna12@live.com>
2022-10-12 11:32:06 -0500

    array and hashmap practice

--- easy/majority-elements.js
+++ easy/majority-elements.js
@@ -1,26 +1,19 @@
+// Pseudocode: Place the number and its count into an object. If a count is more than nums.length / 2, then return that key
+
 function majorityElement(nums) {
-  let object = {};
+  let countingObject = {};
 
   for (let i = 0; i < nums.length; i++) {
     const num = nums[i];
 
-    object[num] ? (object[num] += 1) : (object[num] = 1);
-    if (object[num] > nums.length / 2) {
-      return nums[i];
+    countingObject[num]
+      ? (countingObject[num] += 1)
+      : (countingObject[num] = 1);
+  }
+
+  for (const key in countingObject) {
+    if (countingObject[key] > nums.length / 2) {
+      return key;
     }
-  }
-}
-
-function majorityElement(nums) {
-  let object = {};
-
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-
-    object[num] ? (object[num] += 1) : (object[num] = 1);
-  }
-
-  for (const key in object) {
-    if (object[key] > nums.length / 2) return key;
   }
 }
