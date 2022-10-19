d9523276072bfd10c06d4520cd236bc5ce0a1f0b
Michael <mreyna12@live.com>
2022-10-19 13:16:44 -0500

    bubble sort practice

---
+++ medium/sort-colors.js
@@ -0,0 +1,13 @@
+function sortColors(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - 1 - i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        const temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
+    }
+  }
+
+  return nums;
+}
