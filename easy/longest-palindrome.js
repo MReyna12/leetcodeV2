95f441d29b31ef676071b2803a2293237e1996c9
Michael <mreyna12@live.com>
2022-10-24 11:13:42 -0500

    string practice

--- easy/longest-palindrome.js
+++ easy/longest-palindrome.js
@@ -1,18 +1,21 @@
 // Pseudocode:
 
 function longestPalindrome(s) {
-  let letterCounter = {};
+  // Create a object which will hold the characters and the number of times they appear in the string
+  // Create a count, which is increased each time a character appears an even number of times
+  let characterObject = {};
   let count = 0;
 
   for (let i = 0; i < s.length; i++) {
     const character = s[i];
+    characterObject[character]
+      ? (characterObject[character] += 1)
+      : (characterObject[character] = 1);
 
-    letterCounter[character]
-      ? (letterCounter[character] += 1)
-      : (letterCounter[character] = 1);
-    if (letterCounter[character] % 2 === 0) count += 2;
+    // Palindrome requires the string to read the same left to right and right to left, so we need an even number of characters to create the palindrome.
+    if (characterObject[character] % 2 === 0) count += 2;
   }
 
-  return s.length > count ? count + 1 : count; // If the count is even and the length of the string is larger than count, that means there is at least one letter we could use to increase the palindrome by 1, so we add 1. If count is the same as or greater than
-  // s.length, then we know the longest palindrome we could create is count.
+  // If the count is less than the length of the input string, that means there is at least one character we can grab to place into our palindrome without breaking it. If the count is greater than the length of the string, then we already have the longest palindrome.
+  return s.length > count ? count + 1 : count;
 }
