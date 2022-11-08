35efeacedaa262441f39ac961977725eeb159db4
Michael <mreyna12@live.com>
2022-11-08 11:43:09 -0600

    greedy algo and brute force alternative practice

--- medium/test.js
+++ medium/test.js
@@ -380,3 +380,48 @@ function merge2(leftArray, rightArray) {
 }
 
 mSort2([9, 2, 5, 6, 4, 3, 7, 10, 1, 8]);
+
+// November 8th Practice
+// Greedy Algorithims
+
+// Write a function, makeChange, that returns an integer that represents the least number of coins that add up to an amount where the amount is always divisible by 5.
+const makeChange = (coins, amount) => {
+  coins.sort((a, b) => b - a);
+
+  let coinTotal = 0;
+  let i = 0;
+
+  while (amount > 0) {
+    // Subtract the largest coin possible from the current amount, which is why we sort the coins from largest to smallest.
+    if (coins[i] <= amount) {
+      amount -= coins[i];
+      coinTotal++;
+    } else {
+      i++;
+    }
+  }
+
+  return coinTotal;
+};
+
+makeChange([5, 10, 25], 50);
+
+// Brute Force Alternative w/ Recursion
+const coins = [10, 6, 1];
+
+const makeChangeWithRecursion = (value) => {
+  if (value === 0) return 0;
+
+  let minCoins;
+
+  coins.forEach((coin) => {
+    if (value - coin >= 0) {
+      let currMinCoins = makeChange(value - coin);
+      if (minCoins === undefined || currMinCoins < minCoins) {
+        minCoins = currMinCoins;
+      }
+    }
+  });
+
+  return minCoins + 1;
+};
