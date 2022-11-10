1da60675d7d833ec99db6460f404ce12b1f24889
Michael <mreyna12@live.com>
2022-11-10 13:57:28 -0600

    array searching practice

--- easy/two-sum.js
+++ easy/two-sum.js
@@ -1,18 +1,13 @@
-// P: first param - array of integers. second param - an integer target.
-// R: return the indices of the two numbers such that they add up to the target
-// E: [2, 7, 11, 15], target = 9. Should return [0, 1] because 2 + 7 = 9.
-// P:
-
 function twoSum(nums, target) {
-  // Create an object that will hold an element of the nums array, as a key, and its index, as its value.
-  let indexObject = {};
+  // Create hash table to hold values when looping through array
+  let hashTable = {};
 
-  // For loop that will add an element from the nums array and its index position until two elements equate to the target
+  // Loop through the elements and check if target minus the element exists, if it does it means that current element plus element in hash table equal target
   for (let i = 0; i < nums.length; i++) {
-    if (indexObject[target - nums[i]] !== undefined) {
-      return [indexObject[target - nums[i]], i];
+    if (hashTable[target - nums[i]] !== undefined) {
+      return [hashTable[target - nums[i]], i];
     } else {
-      indexObject[nums[i]] = i;
+      hashTable[nums[i]] = i;
     }
   }
 }
