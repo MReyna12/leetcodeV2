ad4f2a2f476a07e4a1b413b95308a8b1c55bf413
Michael <mreyna12@live.com>
2022-11-10 14:12:59 -0600

    array searching practice

--- easy/majority-elements.js
+++ easy/majority-elements.js
@@ -1,19 +1,21 @@
-// Pseudocode: Place the number and its count into an object. If a count is more than nums.length / 2, then return that key
-
 function majorityElement(nums) {
-  let countingObject = {};
+  // Edge case if nums.length === 1;
+  if (nums.length === 1) return nums[0];
+
+  // Create a hashtable to store the elements from the array and the number of times in which they appear
+  let hashTable = {};
+
+  // Loop through the array, adding the elements and their appearance count to the hashtable. If a element already exists in the hashtable, check if it appears more than Math.floor(n/2).
+  const lengthToBeat = Math.floor(nums.length / 2);
 
   for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-
-    countingObject[num]
-      ? (countingObject[num] += 1)
-      : (countingObject[num] = 1);
-  }
-
-  for (const key in countingObject) {
-    if (countingObject[key] > nums.length / 2) {
-      return key;
+    if (hashTable[nums[i]] === undefined) {
+      hashTable[nums[i]] = 1;
+    } else {
+      hashTable[nums[i]] += 1;
+      if (hashTable[nums[i]] > lengthToBeat) {
+        return nums[i];
+      }
     }
   }
 }
