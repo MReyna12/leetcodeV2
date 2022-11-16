41101ff5897cea60b255e620d4de64a2aa7cc6fb
Michael <mreyna12@live.com>
2022-11-16 11:38:24 -0600

    insertion sort practice

--- easy/insertion-sort.js
+++ easy/insertion-sort.js
@@ -15,4 +15,20 @@ function insertion(array) {
   return array;
 }
 
-insertion([3, 2, 4, 6, 8, 1]);
+function insertionTwo(array) {
+  for (let i = 1; i < array.length; i++) {
+    const currentValue = array[i];
+
+    let leftPointer = i - 1;
+
+    while (leftPointer >= 0 && array[leftPointer] > currentValue) {
+      array[leftPointer + 1] = array[leftPointer];
+      leftPointer--;
+    }
+    array[leftPointer + 1] = currentValue;
+  }
+
+  return array;
+}
+
+insertionTwo([3, 2, 4, 6, 8, 1]);
