62b205fd4ea6afea70a1ca91a2735108e7a68ff4
Michael <mreyna12@live.com>
2022-11-16 11:45:27 -0600

    insertion sort practice

--- easy/insertion-sort.js
+++ easy/insertion-sort.js
@@ -31,4 +31,24 @@ function insertionTwo(array) {
   return array;
 }
 
-insertionTwo([3, 2, 4, 6, 8, 1]);
+function insertionThree(array) {
+  // Create for loop that goes through each value of the array
+  for (let i = 1; i < array.length; i++) {
+    // Create current value that will be moved towards the beginning of the array if its value is less than the value to its left
+    const currentValue = array[i];
+
+    // Create a leftPointer to check how far down the array the currentValue needs to slide
+    let leftPointer = i - 1;
+
+    // While loop that will shift values greater than currentValue over to the right
+    while (leftPointer >= 0 && array[leftPointer] > currentValue) {
+      array[leftPointer + 1] = array[leftPointer];
+      leftPointer--;
+    }
+    array[leftPointer + 1] = currentValue; // If we get to end of array, then this will put the currentValue in the zeroeth index position
+  }
+
+  return array;
+}
+
+insertionThree([3, 2, 4, 6, 8, 1]);
