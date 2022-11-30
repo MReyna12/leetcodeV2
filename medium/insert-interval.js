94e37be9f0e8d9ff76362ee1c113b452c1ec0ca8
Michael <mreyna12@live.com>
2022-11-30 13:47:36 -0600

    array practice

--- medium/insert-interval.js
+++ medium/insert-interval.js
@@ -53,3 +53,26 @@ const insert = (intervals, newInterval) => {
 
   return result;
 };
+
+function insert(intervals, newInterval) {
+  const result = [];
+
+  for (const [start, end] of intervals) {
+    if (!newInterval || end < newInterval[0]) {
+      result.push([start, end]);
+    } else if (newInterval[1] < start) {
+      result.push(newInterval);
+      newInterval = null;
+      result.push([start, end]);
+    } else {
+      newInterval[0] = Math.min(start, newInterval[0]);
+      newInterval[1] = Math.max(end, newInterval[1]);
+    }
+  }
+
+  if (newInterval) {
+    result.push(newInterval);
+  }
+
+  return result;
+}
