c2b51e6541d28bd97bfa634fcfecf5280652e564
Michael <mreyna12@live.com>
2022-12-14 10:59:37 -0600

    string practice

--- easy/valid-parentheses.js
+++ easy/valid-parentheses.js
@@ -27,3 +27,33 @@ function isValid(s) {
   // If the stack is empty at the end of the loop, then return value is true.
   return stack.length === 0;
 }
+
+const isValid = (s) => {
+  // If the string length is odd, then we know for sure that each bracket does not have a corresponding pair
+  if (s.length % 2 === 1) return false;
+
+  // Create hash to containing keys represented by open brackets and their corresponding values equal to the applicable closed brackets
+  let brackets = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  let stack = [];
+
+  // While looping through each character in the input string, check to see if it is an open bracket.
+  // If an open bracket, then add its corresponding closed bracket to an array.
+  // If a closed bracket, then check to see if the last bracket added to the array is equal to the current closed bracket, if not return false.
+  for (let i = 0; i < s.length; i++) {
+    const currentBracket = s[i];
+
+    if (brackets[currentBracket]) {
+      stack.push(brackets[currentBracket]);
+    } else if (stack.pop() !== currentBracket) {
+      return false;
+    }
+  }
+
+  // If the is a match for each open bracket, then the stack should be empty. If not empty, then there is at least one open bracket without a corresponding closed bracket.
+  return stack.length === 0;
+};
