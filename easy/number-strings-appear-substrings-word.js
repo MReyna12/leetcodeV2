a62d62e0c9b2bdf85470bfb014b4249f3e79fced
Michael <mreyna12@live.com>
2023-01-15 14:29:45 -0600

    interview practice

--- easy/number-strings-appear-substrings-word.js
+++ easy/number-strings-appear-substrings-word.js
@@ -8,4 +8,14 @@ function numOfStrings(patterns, word) {
   return substringCount;
 }
 
-numOfStrings(["a", "abc", "bc", "d"], "abc");
+function numOfStrings(patterns, word) {
+  let substringCount = 0;
+
+  for (let i = 0; i < patterns.length; i++) {
+    if (word.split(patterns[i]).length - 1) {
+      substringCount++;
+    }
+  }
+
+  return substringCount;
+}
