fc2caa3e7ab1a318fbb4c226f2a6dfedadceb580
Michael <mreyna12@live.com>
2023-01-16 13:15:41 -0600

    interview prep

---
+++ medium/group-people-given-group-size-belong-to.js
@@ -0,0 +1,22 @@
+function groupThePeople(groupSizes) {
+  let result = [];
+  const groups = {};
+
+  for (let i = 0; i < groupSizes.length; i++) {
+    const num = groupSizes[i];
+
+    if (groups[num]) {
+      groups[num].push(i);
+    } else {
+      groups[num] = [i];
+    }
+
+    if (groups[num].length === num) {
+      result.push(groups[num]);
+      delete groups[num];
+    }
+  }
+  return result;
+}
+
+groupThePeople([3, 3, 3, 3, 3, 1, 3]);
