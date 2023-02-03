20efa0f42a5ab82a14b90b9eb8c35a4e3985a947
Michael <mreyna12@live.com>
2023-02-03 12:09:47 -0600

    daily leetcode

--- easy/two-sum.js
+++ easy/two-sum.js
@@ -29,3 +29,15 @@ function twoSum(nums, target) {
     hash[nums[i]] = i;
   }
 }
+
+const twoSum = (nums, target) => {
+  let hash = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[target - nums[i]] !== undefined) {
+      return [hash[target - nums[i]], i];
+    } else {
+      hash[nums[i]] = i;
+    }
+  }
+};
